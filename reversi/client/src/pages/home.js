909587b6b825e3bc994fc4c5fcd2f3ab4eeeb5dd
alexs-2020 <axsmith2020@gmail.com>
2024-01-28 19:27:48 -0500

    nodeJS setup

---
+++ reversi/client/src/pages/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import { useContext } from "react";                     // <== IMPORT 
+// import { AuthContext } from "../context/auth.context";  // <== IMPORT
+// import { useEffect } from "react";
+import Navbar from "../components/navbar";
+
+
+function Home() {
+  return ( 
+    <div>
+        <Navbar />
+        <h1>  </h1>
+    </div>
+  );}
+  
+export default Home;
+
+
+
+// <span>{user && user.username}</span>
