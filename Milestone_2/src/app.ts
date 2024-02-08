71b9426bb94d8f6b343067c4ba28c339846edb0f
Viraj Jayaraman <jayaraman.v@northeastern.edu>
2024-02-08 18:05:08 -0500

    adding git changes

--- Milestone_2/src/app.ts
+++ Milestone_2/src/app.ts
@@ -4,22 +4,29 @@ import GameController from "./controller/game_controller";
 import * as readlineSync from 'readline-sync';
 
 // Function to get user input for board size 
-// function getBoardSizeFromUser(): number {
-//     const sizeInput: string = readlineSync.question('Enter the size of the board(Even number):');
-//     const size: number = parseInt(sizeInput, 10);
+function getBoardSizeFromUser(): number {
+    const sizeInput: string = readlineSync.question('Enter the size of the board (Even number; Recommended range: 4-12 (recommendation = 8)):');
+    const size: number = parseInt(sizeInput, 10);
+    const isOdd:number = size%2 
+    if (isNaN(size) ||isOdd || size <= 2 || size >= 13){
+      console.log('Invalid input. Please enter a valid number.');
+      return getBoardSizeFromUser(); // Retry input
+    }
   
-//     if (isNaN(size) ) {
-//       console.log('Invalid input. Please enter a valid number.');
-//       return getBoardSizeFromUser(); // Retry input
-//     }
-  
-//     return size;
-//   }
+    return size;
+  }
+  // default board size 
+  let boardSize = 8;
+  // Get the board size from the user
+  const sizeInput: string = readlineSync.question('Do you want to enter a board size No = 0 and Yes = 1?');
+  const wantsToEnterSize: number = parseInt(sizeInput, 10);
+  if (wantsToEnterSize){
+    boardSize= getBoardSizeFromUser()
+  }
 
 
 
-// Get the board size from the user
-const boardSize = 8;
+
 
 // Create instances of Game and ConsoleGameView
 const game = new Game(boardSize);
